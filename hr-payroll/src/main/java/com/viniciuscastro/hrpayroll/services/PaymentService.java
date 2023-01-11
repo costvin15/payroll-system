a7e59f97786cca6de7ef8b702584cafc4fb4764a
Vinicius Costa Castro <vinicius.castro@powtoon.com>
2023-01-11 01:25:35 -0300

    Using OpenFeign instead RestTemplate

--- hr-payroll/src/main/java/com/viniciuscastro/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/viniciuscastro/hrpayroll/services/PaymentService.java
@@ -2,29 +2,25 @@ package com.viniciuscastro.hrpayroll.services;
 
 import com.viniciuscastro.hrpayroll.entities.Payment;
 import com.viniciuscastro.hrpayroll.entities.Worker;
+import com.viniciuscastro.hrpayroll.feignclients.WorkerFeignClient;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class PaymentService {
     @NonNull
-    private RestTemplate restTemplate;
-    @Value("${hr-worker.host}")
-    private String workerHost;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int workedDays) {
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("id", String.valueOf(workerId));
 
-        String url = workerHost + "/workers/{id}";
-        Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDaily_Income(), workedDays);
     }
 }
