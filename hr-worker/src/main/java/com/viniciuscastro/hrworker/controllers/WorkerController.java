ebf4a39be1495a83d327307d97142208a26d2cc1
Vinicius Costa Castro <vinicius.castro@powtoon.com>
2023-01-11 01:44:36 -0300

    Using Ribbon for Load Balacing

--- hr-worker/src/main/java/com/viniciuscastro/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/viniciuscastro/hrworker/controllers/WorkerController.java
@@ -3,6 +3,9 @@ package com.viniciuscastro.hrworker.controllers;
 import com.viniciuscastro.hrworker.entities.Worker;
 import com.viniciuscastro.hrworker.repositories.WorkerRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +16,8 @@ import java.util.Optional;
 @RequestMapping("workers")
 @AllArgsConstructor
 public class WorkerController {
+    private final static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+    private Environment environment;
     private WorkerRepository workerRepository;
 
     @GetMapping
@@ -23,6 +28,8 @@ public class WorkerController {
 
     @GetMapping("{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("Port: " + environment.getProperty("local.server.port"));
+
         Optional<Worker> worker = workerRepository.findById(id);
         return worker.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
