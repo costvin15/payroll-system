c9e9287fc014504a6c3cc88c217c86ae03535321
Vinicius Costa Castro <vinicius.castro@powtoon.com>
2023-01-12 02:37:44 -0300

    Using RefreshScope for Config

--- hr-worker/src/main/java/com/viniciuscastro/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/viniciuscastro/hrworker/controllers/WorkerController.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@RefreshScope
 @RestController
 @RequestMapping("workers")
 @RequiredArgsConstructor
