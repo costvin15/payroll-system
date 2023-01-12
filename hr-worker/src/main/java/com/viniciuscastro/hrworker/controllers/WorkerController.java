ae7168c57059ecbb2cab25f61840ccb2f76aa234
Vinicius Costa Castro <vinicius.castro@powtoon.com>
2023-01-12 02:26:08 -0300

    Retrieving Config data from git repository

--- hr-worker/src/main/java/com/viniciuscastro/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/viniciuscastro/hrworker/controllers/WorkerController.java
@@ -2,7 +2,11 @@ package com.viniciuscastro.hrworker.controllers;
 
 import com.viniciuscastro.hrworker.entities.Worker;
 import com.viniciuscastro.hrworker.repositories.WorkerRepository;
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +15,13 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("workers")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class WorkerController {
+    @NonNull
     private WorkerRepository workerRepository;
+    @Value("${test.config}")
+    private String testConfig;
+    private final Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -25,5 +33,11 @@ public class WorkerController {
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
         Optional<Worker> worker = workerRepository.findById(id);
         return worker.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("configs")
+    public ResponseEntity<Void> getConfigurations() {
+        logger.info("Configuration: " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 }
