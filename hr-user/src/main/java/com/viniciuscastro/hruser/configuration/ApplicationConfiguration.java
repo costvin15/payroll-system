6195930ef275addf3d4a6949ad3edeb171f6a79d
Vinicius Costa Castro <vinicius.castro@powtoon.com>
2023-01-12 03:07:16 -0300

    Initial data added

---
+++ hr-user/src/main/java/com/viniciuscastro/hruser/configuration/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package com.viniciuscastro.hruser.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user (id, name, email, password) VALUES (1, 'Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (id, name, email, password) VALUES (2, 'Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+
+INSERT INTO tb_role (id, role_name) VALUES (1, 'ROLE_OPERATOR');
+INSERT INTO tb_role (id, role_name) VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
